cbd51891f39f84baed872da14dbae1f15e93c381
MarianaMartins01 <marianafmartinsmm209@gmail.com>
2025-01-13 20:00:03 +1300

    Deixando responsivo

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 
 const form = document.querySelector(".form-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
-
+const buttonclik= document.querySelector("button")
 
 function cliqueiNoBotao(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
+    buttonclik.style.visibility = "hidden"
 }
 
 function cliqueiNaMascara(){
     form.style.left = "-300px"
     form.style.transform ="translatex(0)"
     mascara.style.visibility = "hidden"
+    buttonclik.style.visibility ="visible"
 }
